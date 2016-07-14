cbf143ddb62d7dad3c3271124e93b6c3ef1128ce
alisam1 <alisa-m2@yandex.ru>
2016-07-14 08:39:17 +0300

    Factorial

---
+++ HWJavaCore/src/HW/HomeWork01.java
@@ -0,0 +1,10 @@
+package HW;
+
+public class HomeWork01 {
+	public static void main(String[] args) {
+		final Factorial f = new Factorial();
+		final int usernumber = 4;
+		System.out.println("Факториал " + usernumber + " равен " + f.fact(usernumber));
+
+	}
+}
